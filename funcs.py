3b3d2a9eb8f8c4177bd635ee3ff379132875020e
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2024-12-06 05:43:55 -0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -142,9 +142,9 @@ def gme(file, msg, message, username, database):
                     message.reply_document(txtname, thumb=thumb, caption=caption)
                     database[username]["totalUpload"] += file_size
             else:
-                 ink = DV.upload(file)
-                 txtname = os.path.split(file)[1] + ".txt"
-                 with open(txtname, "w") as t:
+                ink = DV.upload(file)
+                txtname = os.path.split(file)[1] + ".txt"
+                with open(txtname, "w") as t:
                       t.write(link)
                       t.close()
                     n = f"__☁️Subida finalizada__\n\n🔗{link}"
